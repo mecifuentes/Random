57014d97436d10cb2e9d49164ac6d6f635539c7c
Manuel Cifuentes <manu@MacBook-Pro-de-Manuel.local>
2022-03-22 22:42:05 -0300

    Webdani

---
+++ web1/hola.py
@@ -0,0 +1,16 @@
+from turtle import onclick
+
+
+print("hello world")
+
+class pato():
+    def __init__(self,a):
+        self.nombre=a
+
+
+def Por_nombre(obj):
+    return obj.nombre
+onj1= pato("juan")
+print(onj1.nombre)
+
+#a=sorted([dasdas,ewqeqw,ewqqw,dasdc,czxcxz],key=Por_nombre)
